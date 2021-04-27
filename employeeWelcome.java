e81dd23345b44e603e61bd57dad0803cd3730cd7
Renuka Sawale <rrsuryawanshi11@gmail.com>
2021-04-28 00:37:13 +0530

    Added Employee wage day reach to month

--- employeeWelcome.java
+++ employeeWelcome.java
@@ -3,27 +3,30 @@ public class employeeWelcome {
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HR = 20;
 	public static final int NO_OF_WORKING_DAYS = 2;
+	public static final int MAX_HRS_IN_MONTH = 10;
 	public static void main(String[] args) {
 		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpWage = 0;
-	    for (int day = 0; day < NO_OF_WORKING_DAYS; day++) {
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		switch (empCheck) {
-			case IS_PART_TIME:
-			empHrs = 4;
-			break;
-			case IS_FULL_TIME:
-			empHrs = 8;
-			break;
-			default:
-				empHrs = 0;
-		}
-		empWage = EMP_RATE_PER_HR * empHrs;
-		totalEmpWage += empWage;
-		System.out.println("Employee Wage is:" + empWage);
-       	      }
-	     System.out.println("Total Employee Wage is:" + totalEmpWage);
+		case IS_PART_TIME:
+		empHrs = 4;
+		break;
+		case IS_FULL_TIME:
+		empHrs = 8;
+		break;
+		default:
+			empHrs = 0;
 	}
+	totalEmpHrs += empHrs;
+	System.out.println("Emp Wage Day:" + totalWorkingDays + "Employee Hr:"  + empHrs);
+}
+		int totalEmpWage = EMP_RATE_PER_HR * totalEmpHrs;
+    System.out.println("Total Employee Wage is:" + totalEmpWage);
+	}
+
 
 }
