f03d550ab9d4cea9e567a5e4b2b32136149edcdd
Renuka Sawale <rrsuryawanshi11@gmail.com>
2021-04-28 00:01:22 +0530

    Added Employee wage daily computation

--- employeeWelcome.java
+++ employeeWelcome.java
@@ -1,10 +1,18 @@
 public class employeeWelcome {
 	public static void main(String[] args) {
+		//Constants
 		int IS_FULL_TIME = 1;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee is present");
-		else
-			System.out.println("Employee is absent");
+		int EMP_RATE_PER_HR = 20;
+		double empCheck=Math.floor(Math.random() * 10) % 2;
+		int empHrs;
+		if(empCheck == IS_FULL_TIME) {
+			empHrs=8;
+	    }
+		else {
+			empHrs=0;
+		}
+		int salary = EMP_RATE_PER_HR*empHrs;
+		System.out.println("Daily Employee Wage Calculation is:" + salary);
 	}
+
 }
