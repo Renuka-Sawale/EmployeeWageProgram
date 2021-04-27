7866f095158d358eab5abb74e59646b34ae29f9d
Renuka Sawale <rrsuryawanshi11@gmail.com>
2021-04-28 00:27:49 +0530

    Added Employee wage computation for a month

--- employeeWelcome.java
+++ employeeWelcome.java
@@ -2,9 +2,12 @@ public class employeeWelcome {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HR = 20;
+	public static final int NO_OF_WORKING_DAYS = 2;
 	public static void main(String[] args) {
 		int empHrs = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
+	    for (int day = 0; day < NO_OF_WORKING_DAYS; day++) {
 		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		switch (empCheck) {
 			case IS_PART_TIME:
@@ -17,8 +20,10 @@ public class employeeWelcome {
 				empHrs = 0;
 		}
 		empWage = EMP_RATE_PER_HR * empHrs;
+		totalEmpWage += empWage;
 		System.out.println("Employee Wage is:" + empWage);
+       	      }
+	     System.out.println("Total Employee Wage is:" + totalEmpWage);
 	}
 
 }
-
