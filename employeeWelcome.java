a3a0c1d16997895a3ed503b4eec78332d51e18da
Renuka Sawale <rrsuryawanshi11@gmail.com>
2021-04-28 00:19:04 +0530

    Added Employee wage using switch statement

--- employeeWelcome.java
+++ employeeWelcome.java
@@ -1,22 +1,23 @@
 public class employeeWelcome {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HR = 20;
 	public static void main(String[] args) {
-		//Constants
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HR = 20;
-		//variables
 		int empHrs = 0;
 		int empWage = 0;
-		double empCheck=Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_PART_TIME) {
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) {
+			case IS_PART_TIME:
 			empHrs = 4;
-	    }
-		else if (empCheck == IS_FULL_TIME)
+			break;
+			case IS_FULL_TIME:
 			empHrs = 8;
-		else
-			empHrs = 0;
+			break;
+			default:
+				empHrs = 0;
+		}
 		empWage = EMP_RATE_PER_HR * empHrs;
-		System.out.println("Part time employee Wage is:" + empWage);
+		System.out.println("Employee Wage is:" + empWage);
 	}
 
 }
